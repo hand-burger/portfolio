84a5014fd914146ca86c4c072bf277bb85ce714d
hand-burger <jtmcash5@gmail.com>
2021-08-30 21:03:12 -0400

    📯 change directory

--- light.js
+++
@@ -1,9 +0,0 @@
-lightBtn = document.getElementById("lightbulb");
-
-// When clicked
-lightBtn.addEventListener("click", () => {
-    document.body.classList.toggle("light");
-    document.getElementById("left").style.backgroundColor = "#202731"
-    document.getElementById("left").style.color = "#f9e7cf"
-    document.getElementById("lightbulb").style.color = "#202731"
-});
